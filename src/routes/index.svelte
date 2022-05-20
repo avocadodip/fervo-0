e3b90cef710559f2b5404b7b882ccb053019b06a
cewidiupleek <cewidiupleek@gmail.com>
2022-05-20 10:46:00 -0500

    app

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,10 @@
 <script>
-  let todos = [];
+  import { todos } from './todos';
+  import Footer from '../components/Footer.svelte'
+
+  const markComplete = () => {
+
+  }
 </script>
 
 <svelte:head>
@@ -13,7 +18,7 @@
 
 <main class="text-white my-[49px] mx-[35px]">
 
-  <section class="flex justify-between items-center font-bold">
+  <section class="flex justify-between items-center font-bold mb-10">
     <div class="">
       <h1 class="text-[50px]">To-do</h1>
       <h2 class="text-[30px]">Due @ 9:00 PM</h2>
@@ -22,23 +27,43 @@
   </section>
 
   <ol>
-    {#each todos as todo}
+    {#each $todos as todo (todo.id)}
       <li>
-        <span class:complete={todo.isComplete}>
-          {todo.task}
-        </span>
-        <span>
-          <button on:click={() => markComplete(todo)}>✓</button>
-          <button on:click={() => deleteTodo(todo.id)}>✗</button>
-        </span>
+        <div class="flex items-center mt-5 mb-2">
+          <div class="circle"/>
+          <h2 class="text-[30px] font-bold mt-2" class:complete={todo.isComplete}>
+            {todo.task}
+          </h2>
+        </div>
+        <p class="mb-16">
+          {todo.desc}
+        </p>
+        {#if todo.id != 3}
+          <hr/>
+        {/if}
       </li>
     {:else}
       <p>No todos found</p>
     {/each}
   </ol>
 
-  <footer>
-
-  </footer>
+  <Footer />
 
 </main>
+
+<style>
+  .circle {
+    border: 3px solid white;    
+    background-color: rgba(255, 255, 255, 0);
+    height: 30px;
+    width: 30px;
+    border-radius: 20px;
+    cursor: pointer;
+    margin-right: 15px;
+  }
+
+  hr {
+    border-top: 5px solid white;
+    border-radius: 20px;
+  }
+</style>
---
+++ src/routes/todos.js
@@ -0,0 +1,22 @@
+import {writable} from 'svelte/store';
+
+export const todos = writable([
+  {
+    id: 1,
+    task: 'Finish Article',
+    desc: 'Become the Snowden of USC',
+    isComplete: false,
+  },
+  {
+    id: 2,
+    task: 'Complete Job Application',
+    desc: 'Cage match with Tim Cook',
+    isComplete: false,
+  },
+  {
+    id: 3,
+    task: 'Release the uPhone',
+    desc: 'Like the iPhone but less self indulgent',
+    isComplete: false,
+  },
+]);
