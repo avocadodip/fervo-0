67a43144f1556db5843b1eb71e8aee5e3d9173e1
cewidiupleek <cewidiupleek@gmail.com>
2022-05-20 20:36:30 -0500

    revert

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,17 @@
 <script>
 // @ts-nocheck
 
-  // imports
-  // import { onMount } from 'svelte';
-  // import { firebaseConfig } from '../lib/firebase';
-  // import { initializeApp } from 'firebase/app';
-  // import {
-  //   getFirestore, collection, onSnapshot,
-  //   addDoc, deleteDoc, doc,
-  //   query, where,
-  //   orderBy, serverTimestamp,
-  //   getDoc, updateDoc
-  // } from 'firebase/firestore'
+  imports
+  import { onMount } from 'svelte';
+  import { firebaseConfig } from '../lib/firebase.js';
+  import { initializeApp } from 'firebase/app';
+  import {
+    getFirestore, collection, onSnapshot,
+    addDoc, deleteDoc, doc,
+    query, where,
+    orderBy, serverTimestamp,
+    getDoc, updateDoc
+  } from 'firebase/firestore'
 
   //variables
   let todos = [];
